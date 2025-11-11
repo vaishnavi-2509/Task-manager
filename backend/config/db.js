abc9c45c7945fa0df7bcf32cdcfc890ec6288790
vaishnavi <agrawalvaishnavi2905@gmail.com>
2025-11-12 00:25:15 +0530

    "mongodb setup"

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Connected to MongoDB")
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
 };
-
 export default connectDB;
